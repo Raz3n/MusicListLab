9a6154bdce469804ecd8c2beef5ecd882edfd3e2
Nelson <nbritob@hotmail.com>
2020-01-28 15:09:55 +0000

    creates file structure and musicbox skeleton

---
+++ src/containers/MusicBox.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class MusicBox extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        songs: []
+    };
+}
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MusicBox;
